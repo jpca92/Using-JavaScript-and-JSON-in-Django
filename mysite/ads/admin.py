6f9e874d7b24ceaba69178926e36c162b815fd45
jpca92 <jpca92@gmail.com>
2024-08-08 02:59:57 +0000

    module 4 works

--- mysite/ads/admin.py
+++ mysite/ads/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from ads.models import Ad
+from ads.models import Ad, Fav
 # Register your models here.
 admin.site.register(Ad)
+admin.site.register(Fav)
--- mysite/ads/models.py
+++ mysite/ads/models.py
@@ -10,15 +10,19 @@ class Ad(models.Model) :
     )
     price = models.DecimalField(max_digits=7, decimal_places=2, null=True)
     text = models.TextField()
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    comments = models.ManyToManyField(settings.AUTH_USER_MODEL,
-                                      through='Comment', related_name='comments_owned')
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='fav_thing_owner')
+    comments = models.ManyToManyField(settings.AUTH_USER_MODEL, through='Comment', related_name='comments_owned')
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     # Picture
     picture = models.BinaryField(null=True, editable=True)
     content_type = models.CharField(max_length=256, null=True, help_text='The MIMEType of the file')
+
+    # Favorites
+    favorites = models.ManyToManyField(settings.AUTH_USER_MODEL,
+        through='Fav', related_name='favorite_ads')
 
     # Shows up in the admin list
     def __str__(self):
@@ -39,3 +43,14 @@ class Comment(models.Model) :
     def __str__(self):
         if len(self.text) < 15 : return self.text
         return self.text[:11] + ' ...'
+
+class Fav(models.Model) :
+    ad = models.ForeignKey(Ad, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='favs_ads')
+
+    # https://docs.djangoproject.com/en/4.2/ref/models/options/#unique-together
+    class Meta:
+        unique_together = ('ad', 'user')
+
+    def __str__(self) :
+        return '%s likes %s'%(self.user.username, self.ad.title[:10])
