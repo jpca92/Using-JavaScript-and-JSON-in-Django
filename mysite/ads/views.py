33514fb2c61d9534a9147d68f2326dc225c57fe9
jpca92 <jpca92@gmail.com>
2024-08-08 03:41:17 +0000

    module 5 search bar works

--- mysite/ads/views.py
+++ mysite/ads/views.py
@@ -12,13 +12,34 @@ from ads.forms import CreateForm, CommentForm
 from ads.models import Ad, Comment, Fav
 from ads.owner import OwnerListView, OwnerDetailView, OwnerCreateView, OwnerUpdateView, OwnerDeleteView
 
+from django.db.models import Q
+from django.contrib.humanize.templatetags.humanize import naturaltime
 
 class AdListView(OwnerListView):
     model = Ad
     template_name = "ads/ad_list.html"
 
     def get(self, request):
-        ad_list = Ad.objects.all()
+        strval = request.GET.get("search", False)
+        if strval :
+            # Simple title-only search
+            # objects = Post.objects.filter(title__contains=strval).select_related().order_by('-updated_at')[:10]
+
+            # Multi-field search
+            query = Q(title__contains=strval)
+            query.add(Q(text__contains=strval), Q.OR)
+            objects = Ad.objects.filter(query).select_related().order_by('-updated_at')[:10]
+        else :
+            # try both versions with > 4 posts and watch the queries that happen
+            objects = Ad.objects.all().order_by('-updated_at')[:10]
+            # objects = Post.objects.select_related().all().order_by('-updated_at')[:10]
+
+        # Augment the post_list
+        for obj in objects:
+            obj.natural_updated = naturaltime(obj.updated_at)
+
+
+        ad_list = objects
         favorites = list()
         if request.user.is_authenticated:
             rows = request.user.favorite_ads.values('id')
